2205e0c6f79e1cc81dd707eedfa77b869ecda585
Maurizio De Santis <desantis.maurizio@gmail.com>
2019-03-06 14:25:40 +0100

    Restrict Playbook layout to html pages only
    It was matching .css files causing a layout error

--- middleman/config.rb
+++ middleman/config.rb
@@ -39,7 +39,7 @@ page '/*.txt', layout: false
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 page '/', layout: 'home'
-page /.+/, layout: 'playbook'
+page /.+\.html$/, layout: 'playbook'
 page '/middleman/admin/*', layout: false
 
 # Proxy pages
