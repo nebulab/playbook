dfaf3aae5a84f8db81393bde73546135715fc038
Michela Frecchiami <michela.frecchiami@gmail.com>
2019-03-26 18:42:49 +0100

    Add offcanvas navigation

--- middleman/assets/javascripts/offcanvas-sidebar.js
+++ middleman/assets/javascripts/offcanvas-sidebar.js
@@ -1,9 +1,14 @@
 window.onload = function() {
-  let container    = document.querySelector('#canvas')
+  let container    = document.querySelector('.canvas')
   let toggleButton = document.querySelector('.nav-button')
+  let asideContainer = document.querySelector('.sidebar')
+  let overlay = document.querySelector('.overlay')
+  let header = document.querySelector('.header')
   
   toggleButton.addEventListener('click', (e) => {
     e.preventDefault();
     container.classList.toggle('show-nav');
+    asideContainer.classList.toggle('show-nav');
+    overlay.classList.toggle('show');
   });
 }
