3d4e30c23adcc75f06097fe9ef11794b601de989
Michela Frecchiami <michela.frecchiami@gmail.com>
2019-03-27 12:53:53 +0100

    Set main navigation on small devices

--- middleman/assets/javascripts/offcanvas-sidebar.js
+++ middleman/assets/javascripts/offcanvas-sidebar.js
@@ -1,14 +1,17 @@
 window.onload = function() {
-  let container    = document.querySelector('.canvas')
+  // let container    = document.querySelector('.canvas')
   let toggleButton = document.querySelector('.nav-button')
-  let asideContainer = document.querySelector('.sidebar')
-  let overlay = document.querySelector('.overlay')
-  let header = document.querySelector('.header')
+  // let asideContainer = document.querySelector('.sidebar')
+  // let overlay = document.querySelector('.overlay')
+  // let header = document.querySelector('.header')
+  
+  let body = document.body
   
   toggleButton.addEventListener('click', (e) => {
     e.preventDefault();
-    container.classList.toggle('show-nav');
-    asideContainer.classList.toggle('show-nav');
-    overlay.classList.toggle('show');
+    body.classList.toggle('js-show-nav');
+    // container.classList.toggle('show-nav');
+    // asideContainer.classList.toggle('show-nav');
+    // overlay.classList.toggle('show');
   });
 }
