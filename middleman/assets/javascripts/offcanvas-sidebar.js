b2b317edcd9b2ab2df19f418417daac5ac320fb0
Michela Frecchiami <michela.frecchiami@gmail.com>
2019-04-04 10:42:51 +0200

    Close sidebar on click outside it, fix cookies msg

--- middleman/assets/javascripts/offcanvas-sidebar.js
+++ middleman/assets/javascripts/offcanvas-sidebar.js
@@ -1,18 +1,18 @@
-
-window.onload = function() {
-  // let container    = document.querySelector('.canvas')
-  let toggleButton = document.querySelector('.nav-button')
-  // let asideContainer = document.querySelector('.sidebar')
-  // let overlay = document.querySelector('.overlay')
-  // let header = document.querySelector('.header')
-  
-  let body = document.body
-  
-  toggleButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    body.classList.toggle('js-show-nav');
-    // container.classList.toggle('show-nav');
-    // asideContainer.classList.toggle('show-nav');
-    // overlay.classList.toggle('show');
+$(document).ready(function() {
+  $('.nav-button').on('click', function() {
+    $('body').toggleClass('js-show-nav');
   });
-}
+});
+$(document).click(function(e) {
+  if ((e.target != $('.nav')[0]) && (e.target != $('.nav-button')[0]) && ($('body').hasClass('js-show-nav'))) {
+    var li_tags = $($('.nav')[0]).find('li');
+    var a_tags = $($('.nav')[0]).find('a');
+    for (var i = 0; i < li_tags.length; i++) {
+      if (e.target == li_tags[i] || e.target == a_tags[i]) {
+        return;
+      }
+    }
+    $('body').toggleClass('js-show-nav');
+  }
+});
+
