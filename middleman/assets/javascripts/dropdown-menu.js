d1359274b9f27ad806c48402fcfa98b5e6bb03ff
Michela Frecchiami <michela.frecchiami@gmail.com>
2019-06-03 13:14:33 +0200

    Adjust dropdown, style next btn, remove old files

--- middleman/assets/javascripts/dropdown-menu.js
+++ middleman/assets/javascripts/dropdown-menu.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
-  if($(".nav-button").is(":visible")) {
-    $('.submenu').hide();
-  };
-
-    $('.nav-list-item').click(function(event) {
+    $('.nav-list-item > a').click(function(event) {
         event.stopPropagation();
-        $('> .submenu', this).toggle();
-        $(this).toggleClass("opened");
+        $(this).parent().find('.submenu').first().fadeToggle('linear');
+        $(this).parent().toggleClass('opened');
     });
-    
+
     $(".nav-button").click(function(){
        $(".sidebar").toggle();
        $("body").toggleClass("js-show-nav");
