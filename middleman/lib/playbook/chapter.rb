f8beea218a79d8a18e394e8330da5f1360a2042a
Matteo Latini <matteolatini@nebulab.it>
2021-04-30 12:56:20 +0200

    Rubocop manualcorrect

--- middleman/lib/playbook/chapter.rb
+++ middleman/lib/playbook/chapter.rb
@@ -1,54 +1,52 @@
 # frozen_string_literal: true
 
-module Playbook
-  class Chapter < SimpleDelegator
-    attr_reader :resource
+class Playbook::Chapter < SimpleDelegator
+  attr_reader :resource
 
-    class << self
-      def all_by_section(section)
-        section.has_many_chapters_values.each_with_object([]) do |section_chapter_value, memo|
-          chapter_resource = find_chapter_resource_by_section_and_section_chapter_value(section, section_chapter_value)
+  class << self
+    def all_by_section(section)
+      section.has_many_chapters_values.each_with_object([]) do |section_chapter_value, memo|
+        chapter_resource = find_chapter_resource_by_section_and_section_chapter_value(section, section_chapter_value)
 
-          memo << new(chapter_resource) if chapter_resource
-        end
-      end
-
-      private
-
-      def find_chapter_resource_by_section_and_section_chapter_value(section, section_chapter_value)
-        section.chapters_resources.find do |chapter_resource|
-          chapter_section_value = chapter_resource.data[section.app.config.playbook_chapter_belongs_to_section_key]
-
-          section_chapter_value == chapter_section_value
-        end
+        memo << new(chapter_resource) if chapter_resource
       end
     end
 
-    def initialize(resource)
-      @resource = resource
-      super @resource
-    end
+    private
 
-    def app
-      resource.instance_variable_get :@app
-    end
+    def find_chapter_resource_by_section_and_section_chapter_value(section, section_chapter_value)
+      section.chapters_resources.find do |chapter_resource|
+        chapter_section_value = chapter_resource.data[section.app.config.playbook_chapter_belongs_to_section_key]
 
-    def section
-      Playbook::Section.find_by_chapter(self)
+        section_chapter_value == chapter_section_value
+      end
     end
+  end
 
-    def next
-      section.chapters[section.chapters.find_index { |chapter| page_id == chapter.page_id }.next]
-    end
+  def initialize(resource)
+    @resource = resource
+    super @resource
+  end
 
-    def title_tag_content
-      return unless section
+  def app
+    resource.instance_variable_get :@app
+  end
 
-      "#{section.data.title} - #{data.title}"
-    end
+  def section
+    Playbook::Section.find_by_chapter(self)
+  end
 
-    def meta_description_tag_content
-      data.meta_description
-    end
+  def next
+    section.chapters[section.chapters.find_index { |chapter| page_id == chapter.page_id }.next]
+  end
+
+  def title_tag_content
+    return unless section
+
+    "#{section.data.title} - #{data.title}"
+  end
+
+  def meta_description_tag_content
+    data.meta_description
   end
 end
--- middleman/lib/playbook/section.rb
+++ middleman/lib/playbook/section.rb
@@ -1,80 +1,78 @@
 # frozen_string_literal: true
 
-module Playbook
-  class Section < SimpleDelegator
-    ROOT_FILES_EXCLUDED_BY_PLAYBOOK_SECTIONS = %w[
-      index.html.md
-    ].freeze
+class Playbook::Section < SimpleDelegator
+  ROOT_FILES_EXCLUDED_BY_PLAYBOOK_SECTIONS = %w[
+    index.html.md
+  ].freeze
 
-    attr_reader :resource
+  attr_reader :resource
 
-    class << self
-      def all(app)
-        sections_files(app).map { |file| new(app.sitemap.find_resource_by_path(app.sitemap.file_to_path(file))) }
-      end
-
-      def find_by_chapter(chapter)
-        all(chapter.app).find { |section| section.chapters_dir == chapter.file_descriptor.relative_path.dirname.to_s }
-      end
-
-      private
-
-      def sections_files(app)
-        app
-          .extensions[:file_watcher]
-          .sources
-          .by_type(:source)
-          .files
-          .select { |file| section_file?(file) }
-      end
-
-      def section_file?(file)
-        file.relative_path.dirname.to_s == '.' &&
-          !ROOT_FILES_EXCLUDED_BY_PLAYBOOK_SECTIONS.include?(file.relative_path.to_s) &&
-          file.relative_path.to_s.end_with?('.html.md')
-      end
+  class << self
+    def all(app)
+      sections_files(app).map { |file| new(app.sitemap.find_resource_by_path(app.sitemap.file_to_path(file))) }
     end
 
-    def initialize(resource)
-      @resource = resource
-      super @resource
+    def find_by_chapter(chapter)
+      all(chapter.app).find { |section| section.chapters_dir == chapter.file_descriptor.relative_path.dirname.to_s }
     end
 
-    def app
-      @resource.instance_variable_get :@app
-    end
+    private
 
-    def chapters
-      Playbook::Chapter.all_by_section(self)
-    end
-
-    def chapters_files
+    def sections_files(app)
       app
         .extensions[:file_watcher]
         .sources
         .by_type(:source)
         .files
-        .select { |file| file.relative_path.dirname.to_s == chapters_dir }
+        .select { |file| section_file?(file) }
     end
 
-    def chapters_resources
-      chapters_files.map { |file| app.sitemap.find_resource_by_path(app.sitemap.file_to_path(file)) }
+    def section_file?(file)
+      file.relative_path.dirname.to_s == '.' &&
+        !ROOT_FILES_EXCLUDED_BY_PLAYBOOK_SECTIONS.include?(file.relative_path.to_s) &&
+        file.relative_path.to_s.end_with?('.html.md')
     end
+  end
 
-    def chapters_dir
-      File.basename path, File.extname(path)
-    end
+  def initialize(resource)
+    @resource = resource
+    super @resource
+  end
 
-    def has_many_chapters_values
-      data[app.config.playbook_section_has_many_chapters_key]
-    end
+  def app
+    @resource.instance_variable_get :@app
+  end
 
-    def social_cover
-      data.social_cover
-    end
+  def chapters
+    Playbook::Chapter.all_by_section(self)
+  end
 
-    def next
-      self.class.all(app).find { |section| section.data.position == data.position.next }
-    end
+  def chapters_files
+    app
+      .extensions[:file_watcher]
+      .sources
+      .by_type(:source)
+      .files
+      .select { |file| file.relative_path.dirname.to_s == chapters_dir }
+  end
+
+  def chapters_resources
+    chapters_files.map { |file| app.sitemap.find_resource_by_path(app.sitemap.file_to_path(file)) }
+  end
+
+  def chapters_dir
+    File.basename path, File.extname(path)
+  end
+
+  def has_many_chapters_values
+    data[app.config.playbook_section_has_many_chapters_key]
+  end
+
+  def social_cover
+    data.social_cover
+  end
+
+  def next
+    self.class.all(app).find { |section| section.data.position == data.position.next }
   end
 end
